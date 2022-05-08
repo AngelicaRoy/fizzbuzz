43fbf0b68d87c1ec58cd66407e3e7389eb21e2e1
AngelicaRoy <ariadna.espinosa@gmail.com>
2022-05-08 13:30:32 -0500

    Agrego libreria test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Read json file ===========================
 const Reader=require("./lib/utils/Reader")
 const ExplorersService=require("./lib/services/explorerService")
+
 const explorers=Reader.readJsonFile("explorers.json")
-//console.log(explorers)
 // Return list of explorers that are in Node
-console.log(ExplorerService.filterByMission(explorers, "node"));
+console.log(ExplorerService.filterByMission(explorers, "node"))
